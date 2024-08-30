3f2a1a0171e71b9dd99a4f941139cb777a02163f
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2024-08-30 15:09:07 -0500

    Update YespowerUrx.c

--- YespowerUrx.c
+++ YespowerUrx.c
@@ -1,31 +1,3 @@
-/*
- * Copyright 2011 ArtForz, 2011-2014 pooler, 2018 The Resistance developers, 2020 The Sugarchain Yumekawa developers
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is loosly based on a tiny portion of pooler's cpuminer scrypt.c.
- */
-
 #include "cpuminer-config.h"
 #include "miner.h"
 
@@ -34,51 +6,111 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
+#include <pthread.h>
 
-int scanhash_urx_yespower(int thr_id, uint32_t *pdata,
-	const uint32_t *ptarget,
-	uint32_t max_nonce, unsigned long *hashes_done)
-{
-	static const yespower_params_t params = {
-		.version = YESPOWER_1_0,
-		.N = 2048,
-		.r = 32,
-		.pers = (const uint8_t *)"UraniumX",
-		.perslen = 8
-	};
-	union {
-		uint8_t u8[8];
-		uint32_t u32[20];
-	} data;
-	union {
-		yespower_binary_t yb;
-		uint32_t u32[7];
-	} hash;
-	uint32_t n = pdata[19] - 1;
-	const uint32_t Htarg = ptarget[7];
-	int i;
+#define NUM_THREADS 8  // Number of threads to use
+#define HALF_THREADS (NUM_THREADS / 2)
 
-	for (i = 0; i < 19; i++)
-		be32enc(&data.u32[i], pdata[i]);
+typedef struct {
+    int thr_id;
+    uint32_t *pdata;
+    const uint32_t *ptarget;
+    uint32_t max_nonce;
+    unsigned long *hashes_done;
+    int randomized;
+} thread_data_t;
 
-	do {
-		be32enc(&data.u32[19], ++n);
+static const yespower_params_t params = {
+    .version = YESPOWER_1_0,
+    .N = 2048,
+    .r = 32,
+    .pers = (const uint8_t *)"UraniumX",
+    .perslen = 8
+};
 
-		if (yespower_tls(data.u8, 80, &params, &hash.yb))
-			abort();
+void *scanhash_urx_yespower_thread(void *arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    int thr_id = data->thr_id;
+    uint32_t *pdata = data->pdata;
+    const uint32_t *ptarget = data->ptarget;
+    uint32_t max_nonce = data->max_nonce;
+    unsigned long *hashes_done = data->hashes_done;
+    int randomized = data->randomized;
 
-		if (le32dec(&hash.u32[7]) <= Htarg) {
-			for (i = 0; i < 7; i++)
-				hash.u32[i] = le32dec(&hash.u32[i]);
-			if (fulltest(hash.u32, ptarget)) {
-				*hashes_done = n - pdata[19] + 1;
-				pdata[19] = n;
-				return 1;
-			}
-		}
-	} while (n < max_nonce && !work_restart[thr_id].restart);
+    union {
+        uint8_t u8[80];
+        uint32_t u32[20];
+    } hash_data;
 
-	*hashes_done = n - pdata[19] + 1;
-	pdata[19] = n;
-	return 0;
+    union {
+        yespower_binary_t yb;
+        uint32_t u32[8];
+    } hash;
+
+    uint32_t n = pdata[19] - 1;
+    const uint32_t Htarg = ptarget[7];
+    int i;
+
+    for (i = 0; i < 19; i++)
+        be32enc(&hash_data.u32[i], pdata[i]);
+
+    do {
+        uint32_t nonce = randomized ? rand() : ++n;
+        be32enc(&hash_data.u32[19], nonce);
+
+        if (yespower_tls(hash_data.u8, 80, &params, &hash.yb))
+            abort();
+
+        if (le32dec(&hash.u32[7]) <= Htarg) {
+            for (i = 0; i < 7; i++)
+                hash.u32[i] = le32dec(&hash.u32[i]);
+            if (fulltest(hash.u32, ptarget)) {
+                *hashes_done = nonce - pdata[19] + 1;
+                pdata[19] = nonce;
+                return (void *)1;
+            }
+        }
+    } while (n < max_nonce && !work_restart[thr_id].restart);
+
+    *hashes_done = n - pdata[19] + 1;
+    pdata[19] = n;
+    return (void *)0;
+}
+
+int main(int argc, char **argv) {
+    pthread_t threads[NUM_THREADS];
+    thread_data_t thread_data[NUM_THREADS];
+    uint32_t pdata[20] = {0};  // Initialize with appropriate values
+    uint32_t ptarget[8] = {0}; // Initialize with appropriate values
+    unsigned long hashes_done[NUM_THREADS] = {0};
+    uint32_t max_nonce = 0xFFFFFFFF;
+
+    // Initialize pdata and ptarget with actual values
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data[i].thr_id = i;
+        thread_data[i].pdata = pdata;
+        thread_data[i].ptarget = ptarget;
+        thread_data[i].max_nonce = max_nonce;
+        thread_data[i].hashes_done = &hashes_done[i];
+        thread_data[i].randomized = (i >= HALF_THREADS) ? 1 : 0;
+
+        if (pthread_create(&threads[i], NULL, scanhash_urx_yespower_thread, &thread_data[i]) != 0) {
+            perror("pthread_create");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        void *result;
+        if (pthread_join(threads[i], &result) != 0) {
+            perror("pthread_join");
+            return 1;
+        }
+        if ((int)result == 1) {
+            printf("Thread %d found a valid hash\n", i);
+        }
+    }
+
+    return 0;
 }
